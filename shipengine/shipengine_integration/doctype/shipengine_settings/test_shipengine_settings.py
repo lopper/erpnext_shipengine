78fe1cb73f6757a7f5d8ef1ce7c51e90dea4c82b
Patrick <patricklo@gmail.com>
2024-07-27 00:14:36 +0000

    Add shipping label doc type

---
+++ shipengine/shipengine_integration/doctype/shipengine_settings/test_shipengine_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, p and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestShipEngineSettings(FrappeTestCase):
+	pass
